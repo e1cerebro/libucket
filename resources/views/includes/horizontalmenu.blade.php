f001a0e3b2316246e6559020826d415c6bc1b739
Nwachukwu Christian <nwachukwu16@gmail.com>
2017-02-21 16:32:52 +0000

    First commit: installed the templates and created the master layout

---
+++ resources/views/includes/horizontalmenu.blade.php
@@ -0,0 +1,285 @@
+<div id="horizontal-menu" class="navbar navbar-inverse hidden-sm hidden-xs inner">
+				<div class="container">
+					<div class="navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li>
+								<a href="index-2.html">
+									Home
+								</a>
+							</li>
+							<li class="dropdown active">
+								<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+									Layouts <i class="icon-arrow"></i>
+								</a>
+								<ul class="dropdown-menu">
+									<li class="dropdown-header">
+										Horizontal Menu
+									</li>
+									<li class="active">
+										<a href="layouts_horizontal_menu.html">
+											Horizontal Menu
+										</a>
+									</li>
+									<li>
+										<a href="layouts_horizontal_menu_fixed.html">
+											Horizontal Menu Fixed
+										</a>
+									</li>
+									<li>
+										<a href="layouts_horizontal_sidebar_menu.html">
+											Horizontal &amp; Sidebar Menu
+										</a>
+									</li>
+									<li class="divider"></li>
+									<li>
+										<a href="layouts_sidebar_closed.html">
+											<span class="title"> Sidebar Closed </span>
+										</a>
+									</li>
+									<li>
+										<a href="layouts_boxed_layout.html">
+											<span class="title"> Boxed Layout </span>
+										</a>
+									</li>
+									<li>
+										<a href="layouts_footer_fixed.html">
+											<span class="title"> Footer Fixed </span>
+										</a>
+									</li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+									UI Lab <i class="icon-arrow"></i>
+								</a>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="ui_elements.html">
+											<span class="title"> Elements </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_buttons.html">
+											<span class="title"> Buttons </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_icons.html">
+											<span class="title"> Icons </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_animations.html">
+											<span class="title"> CSS3 Animation </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_subview.html">
+											<span class="title"> Subview </span> <span class="label partition-blue pull-right ">HOT</span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_modals.html">
+											<span class="title"> Extended Modals </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_tabs_accordions.html">
+											<span class="title"> Tabs &amp; Accordions </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_notifications.html">
+											<span class="title"> Notifications </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_sliders.html">
+											<span class="title"> Sliders </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_treeview.html">
+											<span class="title"> Treeview </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_nestable.html">
+											<span class="title"> Nestable List </span>
+										</a>
+									</li>
+									<li>
+										<a href="ui_typography.html">
+											<span class="title"> Typography </span>
+										</a>
+									</li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+									Tables <i class="icon-arrow"></i>
+								</a>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="table_basic.html">
+											<span class="title">Basic Tables</span>
+										</a>
+									</li>
+									<li>
+										<a href="table_responsive.html">
+											<span class="title">Responsive Tables</span>
+										</a>
+									</li>
+									<li>
+										<a href="table_data.html">
+											<span class="title">Advanced Data Tables</span>
+										</a>
+									</li>
+									<li>
+										<a href="table_export.html">
+											<span class="title">Table Export</span>
+										</a>
+									</li>
+								</ul>
+							</li>
+							<li class="dropdown mega-menu">
+								<a class="dropdown-toggle" href="#" data-toggle="dropdown" data-hover="dropdown">
+									Mega Menu <i class="icon-arrow"></i>
+								</a>
+								<ul class="dropdown-menu">
+									<li>
+										<!-- start: MEGA MENU CONTENT -->
+										<div class="mega-menu-content">
+											<div class="row">
+												<div class="col-md-3">
+													<ul class="mega-sub-menu">
+														<li>
+															<span class="mega-menu-sub-title">Forms</span>
+															<ul class="mega-sub-menu">
+																<li class="active">
+																	<a href="form_elements.html">
+																		<span class="title">Form Elements</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="form_wizard.html">
+																		<span class="title">Form Wizard</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="form_validation.html">
+																		<span class="title">Form Validation</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="form_inline.html">
+																		<span class="title">Inline Editor</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="form_x_editable.html">
+																		<span class="title">Form X-editable</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+													</ul>
+												</div>
+												<div class="col-md-3">
+													<ul class="mega-sub-menu">
+														<li>
+															<span class="mega-menu-sub-title">File Upload</span>
+															<ul class="mega-sub-menu">
+																<li>
+																	<a href="form_image_cropping.html">
+																		<span class="title">Image Cropping</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="form_multiple_upload.html">
+																		<span class="title">Multiple File Upload</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="form_dropzone.html">
+																		<span class="title">Dropzone File Upload</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+													</ul>
+												</div>
+												<div class="col-md-3">
+													<ul class="mega-sub-menu">
+														<li>
+															<span class="mega-menu-sub-title">Login</span>
+															<ul class="mega-sub-menu">
+																<li>
+																	<a href="login_login.html">
+																		<span class="title"> Login Form </span>
+																	</a>
+																</li>
+																<li>
+																	<a href="login_login6cf4.html?box=register">
+																		<span class="title"> Registration Form </span>
+																	</a>
+																</li>
+																<li>
+																	<a href="login_login6255.html?box=forgot">
+																		<span class="title"> Forgot Password Form </span>
+																	</a>
+																</li>
+																<li>
+																	<a href="login_lock_screen.html">
+																		<span class="title">Lock Screen</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+													</ul>
+												</div>
+												<div class="col-md-3">
+													<ul class="mega-sub-menu">
+														<li>
+															<span class="mega-menu-sub-title">Pages</span>
+															<ul class="mega-sub-menu">
+																<li>
+																	<a href="pages_user_profile.html">
+																		<span class="title">User Profile</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="pages_invoice.html">
+																		<span class="title">Invoice</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="pages_gallery.html">
+																		<span class="title">Gallery</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="pages_timeline.html">
+																		<span class="title">Timeline</span>
+																	</a>
+																</li>
+																<li>
+																	<a href="pages_calendar.html">
+																		<span class="title">Calendar</span>
+																	</a>
+																</li>
+															</ul>
+														</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+										<!-- end: MEGA MENU CONTENT -->
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<!--/.nav-collapse -->
+				</div>
+			</div>
